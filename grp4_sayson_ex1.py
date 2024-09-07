daa8e918efd179add30f896662e38405070cf5ce
CylenSayson <jeya6064@gmail.com>
2024-09-07 09:40:34 +0800

    Add files via upload

---
+++ grp4_sayson_ex1.py
@@ -0,0 +1,25 @@
+#My first simple script
+print ("Hello,I,m Cylen Sayson! BSCS 3F")
+age = 21
+name = "Cylen"
+
+print("Age: ",age)
+print("Name: ",name)
+
+hadje = "hadje"
+mark = "mark"
+rochelle = "rochelle"
+cylen = "rafael"
+
+print (hadje)
+print (mark)
+print (rochelle)
+print (cylen)
+
+age,name,gender = 21 , "cylen" ,"female"
+print ("Age:",age)
+print ("Name:",name)
+print ("Gender:",gender)
+
+age,edad,num = 21 ,21, 21
+print(age,edad,num)
